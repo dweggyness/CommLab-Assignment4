01d9a5d5b22ce1b435d6f2eac1f38e7bbaf4f3b0
dweggy <colacolaojm@hotmail.com>
2021-11-28 01:13:42 +0400

    use dummy videos to test

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 
 const step2Title = document.querySelector('.step2Title');
 
+// code stolen/modified from internet for scrambling the title to 'Murder'
+// https://codemyui.com/decryption-effect-click/
 var dictionary = "0123456789qwertyuiopasdfghjklzxcvbnm!?></\a`~+*=@#$%".split('');
 
 var ran = function() {
@@ -45,6 +47,7 @@ var startScramble = function(str) {
   }, 32);
 }
 
+///////////////////////////////////
 
 
 // show alert modal, lock user out of doing anything until they press OK
@@ -63,6 +66,7 @@ function showAlert() {
   })
 }
 
+
 // once user closes the alert, make step 3 visible
 function closeAlert() {
   $('.overlay').css('display', 'none')
@@ -79,11 +83,16 @@ function edgify() {
   // want to change the css only after the text is unscrambled
   setTimeout(() => {
     $('body').css('color', '#333333');
-    $('body').css('background-color', "salmon");
+    $('body').css('background-color', "#f2a096");
     $('.altblock').css('background-color', '#a83e32');
     $('#header_container').css('background-color', '#a83e32');
-    $('#hs_submit').css('background-color', 'salmon');
+    $('#hs_submit').css('background-color', '#f2a096');
     $('#sidebar').css('color', "#444444");
+
+    // what are those filters for? 
+    // answer: the images/logo come in one color, but i want to change their colour to something else
+    // so i use a filter to change it, using this codepen to generate the filters
+    // https://codepen.io/sosuke/pen/Pjoqqp
     $('.nav_item .nav_icon').css('filter', "brightness(0) saturate(100%) invert(0%) sepia(10%) saturate(1751%) hue-rotate(322deg) brightness(99%) contrast(87%)");
     $('.nav_item .nav').css('color', "#111")
     $('.sp_helpful_icon_star').css('filter', "brightness(0) saturate(100%) invert(25%) sepia(47%) saturate(2746%) hue-rotate(343deg) brightness(89%) contrast(79%)")
@@ -94,11 +103,60 @@ function edgify() {
   }, 10500)
 }
 
-
-function init() {
-  console.log('hi');
-
-  setTimeout(() => edgify(), 5000);
+function getFrameID(id){
+  var elem = document.getElementById(id);
+  if (elem) {
+      if(/^iframe$/i.test(elem.tagName)) return id; //Frame, OK
+      // else: Look for frame
+      var elems = elem.getElementsByTagName("iframe");
+      if (!elems.length) return null; //No iframe found, FAILURE
+      for (var i=0; i<elems.length; i++) {
+         if (/^https?:\/\/(?:www\.)?youtube(?:-nocookie)?\.com(\/|$)/i.test(elems[i].src)) break;
+      }
+      elem = elems[i]; //The only, or the best iFrame
+      if (elem.id) return elem.id; //Existing ID, return it
+      // else: Create a new ID
+      do { //Keep postfixing `-frame` until the ID is unique
+          id += "-frame";
+      } while (document.getElementById(id));
+      elem.id = id;
+      return id;
+  }
+  // If no element, return null.
+  return null;
 }
 
-init();
+
+
+/// youtube iframe event listeners... so complicated for wat
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('step2YoutubePlayer', {
+    events: {
+      'onReady': onPlayerReady,
+    }
+  })
+}
+
+// This function will be called when the API is fully loaded
+function onPlayerReady(e) {
+  // check the video time every 1 second.
+  const timer = setInterval(() => {
+    const curVideoTime = player.getCurrentTime()
+
+    // once it reaches the part jun is about to be murdered by dania, start edgifying the site
+    if (curVideoTime >= 30 && curVideoTime <= 33) {
+      edgify();
+      clearInterval(timer);
+    }
+  }, 1000)
+}
+
+// Load YouTube Frame API
+(function() { // Closure, to not leak to the scope
+  var s = document.createElement("script");
+  s.src = (location.protocol == 'https:' ? 'https' : 'http') + "://www.youtube.com/player_api";
+  var before = document.getElementsByTagName("script")[0];
+  before.parentNode.insertBefore(s, before);
+})();
